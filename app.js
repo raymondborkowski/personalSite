7df10f67d678e040f58e23eb9de708ca721fd74e
raymondborkowski <rborkows@umich.edu>
2015-12-21 21:56:55 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+f(function(){
+	var app = angular.module('store',[]);
+	app.controller('StoreController',function(){
+
+	});
+
+	var gem = {
+		name:'Dodecahedron',
+		price: 2.95,
+		description: '...',
+	}
+})();
